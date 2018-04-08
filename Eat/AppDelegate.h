26dc5729085a78861a06d8b77aef0ba720654c90
Haris Muharemovic <harismuharemovic@hariss-macbook-pro.local>
2018-04-08 10:46:02 +0400

    Initial Commit

---
+++ Eat/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Eat
+//
+//  Created by Haris Muharemovic on 08/04/2018.
+//  Copyright © 2018 Haris Muharemovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Eat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Eat
+//
+//  Created by Haris Muharemovic on 08/04/2018.
+//  Copyright © 2018 Haris Muharemovic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
